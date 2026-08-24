8a700448cc158341278ccc3e07b6291e387ea1e6
lukaskocian <99292682+lukaskocian@users.noreply.github.com>
2026-08-24 09:53:17 +0200

    added safety catch

--- data-pipeline/sql/refresh_top_events.sql
+++ data-pipeline/sql/refresh_top_events.sql
@@ -57,7 +57,7 @@ INSERT INTO top_events (
 WITH number_of_articles_per_timeframe AS (
     SELECT
         global_event_id,
-        SUM(articles_count) AS articles_1w,
+        SUM(articles_count) FILTER (WHERE time_window_15min >= NOW() - INTERVAL '1 week') AS articles_1w, -- articles_table_15_min should not have more than 1week of data but this FILTER is for safety
         SUM(articles_count) FILTER (WHERE time_window_15min >= NOW() - INTERVAL '1 day') as articles_1d,
         SUM(articles_count) FILTER (WHERE time_window_15min >= NOW() - INTERVAL '1 hour') as articles_1h,
         MAX(action_geo_country_code) AS action_geo_country_code,
